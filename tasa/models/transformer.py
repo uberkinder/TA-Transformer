7bc86ca31f00a345a6f0abae6510d6ee7a5aa1fd
Bogdan Gromov <uberkinder@ya.ru>
2020-12-06 04:51:18 +0300

    [fix] comment

--- tasa/models/transformer.py
+++ tasa/models/transformer.py
@@ -7,7 +7,7 @@ from tasa.utils import d
 
 
 class TATransformer(nn.Module):
-    """Time-Aware Transformer 2"""
+    """Time-Aware Transformer"""
 
     def __init__(self,
                  emb: int, 
