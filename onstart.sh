2afa9a82339135eff1c0a731ab082e2b03f0c7db
BOGDAN <uberkinder@ya.ru>
2021-01-28 10:15:58 +0300

    [fix] onstart

--- onstart.sh
+++ onstart.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-# This file is run on instance start. Output in ./onstart.log
 export DEBIAN_FRONTEND=noninteractive | apt-get -q -y install git
 git clone https://github.com/uberkinder/TA-Transformer.git
